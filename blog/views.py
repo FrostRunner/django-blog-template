3ac4f26548524664dfa00062b2e90386ab50fbd5
Sergey Fomin <PivoWar89@gmail.com>
2023-04-28 19:15:33 +0300

    Added reverse to model of blog Added slug format for URL

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,14 @@ def post_list(request):
     )
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status=Post.Status.PUBLISHED,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
 
     return render(request, "blog/post/detail.html", {"post": post})
