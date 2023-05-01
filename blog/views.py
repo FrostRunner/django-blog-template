5fccc39529545af2b84695a1033c53a229dacba4
Sergey Fomin <PivoWar89@gmail.com>
2023-05-01 18:23:24 +0300

    Add possibility to add forms(draft)

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,43 @@
 from django.core.paginator import EmptyPage
-from django.core.paginator import Paginator
 from django.core.paginator import PageNotAnInteger
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # ... send e-mail
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        "blog/post/share.html",
+        {
+            "post": post,
+            "form": form
+        }
+    )
 
 
 def post_list(request):
